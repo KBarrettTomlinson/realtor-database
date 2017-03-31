60e22c19736d1998c2e30a2160233a793372779c
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-31 16:42:43 -0500

    starts app.js setup

---
+++ server/app.js
@@ -0,0 +1,12 @@
+//requires
+var express = require( 'express' );
+var bodyParser = require( 'body-parser' );
+var path = require( 'path' );
+var mongoose = require( 'mongoose' );
+var index = require('/routes/index.js');
+//globals
+var app = express();
+
+//uses
+
+//listens
