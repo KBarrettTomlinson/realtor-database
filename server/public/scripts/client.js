d4c7bba521a1b44196fe1249aef7cd864012a2f3
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-01 14:59:11 -0500

    retrieves all data from mongo through to client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,24 @@
 $(document).ready(function(){
   console.log("I'm here for you.");
   console.log("good work buddy, grunt is grunting!");
+  getListings();
+
 });
+
+//REST
+  //GETS
+  function getListings(){
+    $.ajax({
+      type    : 'GET',
+      url     : '/realty',
+      success : function(response){
+        console.log( "I've returned, and I've brought you this getListings:",response);
+      }//ends success
+    });//ends ajax object
+  }//ends getListings
+
+  //POSTS
+
+  //PUTS
+
+  //DELETES
