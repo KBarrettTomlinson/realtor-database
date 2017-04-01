deabcaa6a48cfc54271f1ca77ab5d812f0273165
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-01 11:43:21 -0500

    add back end server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,39 @@
-//requires
+//general modules
 var express = require( 'express' );
-var bodyParser = require( 'body-parser' );
-var path = require( 'path' );
-var mongoose = require( 'mongoose' );
-var index = require('/routes/index.js');
-//globals
 var app = express();
+var bodyParser = require( 'body-parser' );
+
+//route variables
+var index = require('./routes/index.js');
+// var realty = require('./routes/realty.js');
+
+//database variables
+// var mongoose = require( 'mongoose' );
+// var mongoURI = "mongodb://localhost:27017/company";
+// var MongoDB = mongoose.connect(mongoURI).connection;
+
+//conneciton error
+// MongoDB.on("error", function(err){
+//   console.log("Mongo Connection Error :" + err);
+// });
+
+//connection success
+// MongoDB.once("open", function(){
+//   console.log("Mongo and I had a long coffee, we feel good about eachother now");
+// });
+
+//set port
+app.set("port", (process.env.PORT || 5000));
+
+//middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("./server/public/"));
 
 //uses
+app.use('/',index);
+// app.use('/realty',realty);
 
 //listens
+app.listen(app.get("port"), function(){
+    console.log("I'm listening for you: " + app.get("port"));
+});
---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+//requires
+var express = require( 'express' );
+var path = require( 'path' );
+
+//globals
+var router = express.Router();
+
+//gets
+router.get( '/', function ( req, res){
+  console.log(" sending back index.html for a / request");
+  res.sendFile( path.resolve( 'server/public/views/index.html' ));
+});//ends router get for /
+
+//exports
+exports = module.router;
