38170d2320a4bdcd3e835fc9ae77b6ee6f805b07
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-01 12:33:12 -0500

    sets up grunt

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    //adds uglify
+    // uglify: {
+    //   client: {
+    //     src: 'client/scripts/client.js',
+    //     dest: 'server/public/scripts/client.min.js'
+    //   }
+    // },
+
+    //add copy
+    copy: {
+      jquery: {
+        expand: true,
+        cwd: 'node_modules/jquery/dist/',
+        src: ['jquery.js'],
+        dest: 'server/public/vendors/'
+      },
+      bootstrap: {
+        expand: true,
+        cwd: "node_modules/bootstrap/dist/",
+        src: ["css/*.*","fonts/*.*","js/*.*"],
+        dest: "server/public/vendors/bootstrap"
+      },
+      views: {
+        expand: true,
+        cwd: 'client/views/',
+        src: ['*.*'],
+        dest: 'server/public/views/'
+      },
+      scripts: {
+        expand: true,
+        cwd: 'client/scripts/',
+        src: ['*.*'],
+        dest: 'server/public/scripts/'
+      },
+    },
+
+    //add watch client side development - includes livereload
+    watch: {
+      options: {
+        livereload: true,},
+      client:{
+        files: ['client/scripts/*.*', 'client/views/*.*'],
+        // What tasks should I complete
+        // tasks: ['uglify', 'copy'],},
+        tasks: ['copy'],},
+    }
+  });//end grunt.initConfig
+
+  //grunt loan plugins
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //grunt run plugins
+  // grunt.registerTask('default', ['uglify','copy','watch']);
+  grunt.registerTask('default', ['copy','watch']);
+}; //end export
---
+++ client/scripts/client.js
@@ -0,0 +1,4 @@
+//doc ready
+$(document).ready(function(){
+  console.log("I'm here for you.");
+});
